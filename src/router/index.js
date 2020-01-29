2312d44b9e496ad2bfe7c3a581d02603f474ac44
Bohdan Illiashyk <bohdan.illiashyk@opteck.local>
2020-01-29 18:23:37 +0200

    add new project

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,28 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/login",
+    name: "login",
+    meta: { layout: "empty" },
+    component: () => import("../views/Login.vue")
+  },
+  {
+    path: "/register",
+    name: "register",
+    meta: { layout: "empty" },
+    component: () => import("../views/Register.vue")
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    meta: { layout: "main" },
+    component: () => import("../views/Categories.vue")
+  },
+  {
+    path: "/detail",
+    name: "detail",
+    meta: { layout: "main" },
+    component: () => import("../views/Detail.vue")
   }
 ];
 
